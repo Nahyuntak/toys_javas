10cb4c55fa929545a1131c36e751aa4d79e4c35a
dancingkimDH <kimdonghyun0701@gmail.com>
2023-05-31 18:35:34 +0900

    scan

--- src/PollScanners.java
+++ src/PollScanners.java
@@ -1,11 +1,23 @@
 public class PollScanners {
   
-    public String[] answer(String A) {
-        String[] result = {"", "", "", "", ""}; 
+
+    
+
+    public int [] answer(int A) {
+        PollsWithMethod pollswithmethod = new PollsWithMethod();
+        PollScanners pollscanners = new PollScanners();
+        int [] result = new int[4];
+        int first = pollscanners.getfirst();
+        
+        answer[] = result [a];
         return result;
     }
 
-
+    public String result(int B) {
+        
+        
+        return result;
+    }
 
 
 }
