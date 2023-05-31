f45e3525b09fe98bcf1d3b71dd9f6c509132b423
dancingkimDH <kimdonghyun0701@gmail.com>
2023-05-31 18:56:37 +0900

    scan

--- src/PollScanners.java
+++ src/PollScanners.java
@@ -1,11 +1,10 @@
 public class PollScanners {
   
- 
 
     public int [] reply(int A) {
         PollsWithMethod pollswithmethod = new PollsWithMethod();
         PollScanners pollscanners = new PollScanners();
-        int[] result = new int[4];
+        int[] result = pollswithmethod.answer;
         String[] answer = {"(1) 전혀 아니다", "(2) 아니다", "(3) 그렇다", "(4) 매우 그렇다"};
 
 
